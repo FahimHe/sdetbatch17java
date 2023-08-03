372a9b0e16cbbe56827c0b5f944b001afa956a53
FahimHe <fahim.hed@gmail.com>
2023-08-02 18:24:32 -0700

    git last class

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int month=15;
+
+        switch (month) {
+
+            case 1:
+                System.out.println("This is january");
+                break;
+            case 2:
+                System.out.println("This is february");
+                break;
+            case 3:
+                System.out.println("This is march");
+                break;
+            case 4:
+                System.out.println("This is april");
+                break;
+            case 5:
+                System.out.println("This is may");
+                break;
+            case 6:
+                System.out.println("This is jun");
+                break;
+            case 7:
+                System.out.println("This is july");
+                break;
+            case 8:
+                System.out.println("This is august");
+                break;
+            case 9:
+                System.out.println("This is september");
+                break;
+            case 10:
+                System.out.println("This is october");
+                break;
+            case 11:
+                System.out.println("This is november");
+                break;
+            case 12:
+                System.out.println("This is december");
+                break;
+            default:
+                System.out.println("This month only exist on Mars, so good luck to you !!!");
+
+            }
+
+        }
+}
